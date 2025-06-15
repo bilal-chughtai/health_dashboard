7de91bb57816b8a181431aacba29240e7ec9794d
Bilal Chughtai <brchughtaii@gmail.com>
2025-06-15 12:47:56 +0100

    fix deployment

--- dashboard/dashboard_personal.py
+++ dashboard/dashboard_personal.py
@@ -1,6 +1,9 @@
 # Boilerplate as streamlit doesn't support running multiple apps from the same file
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import dashboard.dashboard as dashboard
+import dashboard.dashboard as dashboard_frontend
 
 if __name__ == "__main__":
-    dashboard.main()
+    dashboard_frontend.main()
